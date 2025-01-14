ad023ee01232f8978c3ceb9d99a0b4bbd6b5624a
Joel Vestin <joel.vestin06@skola.stenungsund.se>
2025-01-14 15:30:04 +0100

    :)

--- Blackjack.py
+++ Blackjack.py
@@ -1,8 +1,9 @@
 import random
 
-kortlek = (list(range(1, 14)) * 4)
+kortlek = (list((range(1, 14)) * 4) *6)
 random.shuffle(kortlek)
 
+print(kortlek)
 draget_kort = kortlek.pop()
 
 print("Draget kort:",draget_kort) 
