b26b19e980f923a2214be8bd4166f9eee630c76e
Joel Vestin <joel.vestin06@skola.stenungsund.se>
2025-01-28 14:43:32 +0100

    :)

--- Blackjack.py
+++ Blackjack.py
@@ -1,5 +1,15 @@
 import random
 
+def beräkna_summa(hand):
+    summa = sum(hand)
+    antal_ess = hand.count(1)
+
+    while antal_ess > 0 and summa + 10 <= 21:
+        summa += 10
+        antal_ess -= 1
+
+    return summa
+
 kortlek = (list(range(1, 14)) * 4) * 6
 random.shuffle(kortlek)
 
@@ -7,10 +17,11 @@ spelarens_kort = [kortlek.pop(), kortlek.pop()]
 dator_kort_synligt = kortlek.pop()
 dator_kort_hemligt = kortlek.pop()
 
-print("Spelarens kort:", spelarens_kort)
-print("Datorns kort:", dator_kort_synligt)
+spelarens_summa = beräkna_summa(spelarens_kort)
+dator_summa = beräkna_summa([dator_kort_synligt, dator_kort_hemligt])
 
-dator_summa = dator_kort_synligt + dator_kort_hemligt
+print("Spelarens kort:", spelarens_kort, "Summa:", spelarens_summa)
+print("Datorns kort:", [dator_kort_synligt], "+ ?")
 
 if dator_summa == 21:
     print("Datorn har 21! Spelet är slut.")
