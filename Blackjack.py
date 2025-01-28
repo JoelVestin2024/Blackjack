5fdacea336fec13d6b4ff098f4ba209f05f644b5
Joel Vestin <joel.vestin06@skola.stenungsund.se>
2025-01-28 14:24:00 +0100

    :)

--- Blackjack.py
+++ Blackjack.py
@@ -4,7 +4,8 @@ kortlek = (list(range(1, 14)) * 4) * 6
 random.shuffle(kortlek)
 
 spelarens_kort = [kortlek.pop(), kortlek.pop()]
-datorns_kort = kortlek.pop()
+dator_kort_synligt = kortlek.pop()
+dator_kort_hemligt = kortlek.pop()
 
 print("Spelarens kort:", spelarens_kort)
-print("Datorns kort:", datorns_kort)
+print("Datorns kort:", dator_kort_synligt)
