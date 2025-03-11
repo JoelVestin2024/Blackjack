423e321b269de0685ed2b31343faf8372163fddf
Joel Vestin <joel.vestin06@skola.stenungsund.se>
2025-03-11 14:48:57 +0000

    :)

--- Blackjack.py
+++ Blackjack.py
@@ -19,56 +19,59 @@ def beräkna_summa(hand):
 
     return summa
 
-kortlek = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"] * 24
-random.shuffle(kortlek)
+def spela_blackjack():
+    kortlek = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"] * 24
+    random.shuffle(kortlek)
 
-spelarens_kort = [kortlek.pop(), kortlek.pop()]
-datorns_kort = [kortlek.pop(), kortlek.pop()]
+    while True:
+        spelarens_kort = [kortlek.pop(), kortlek.pop()]
+        datorns_kort = [kortlek.pop(), kortlek.pop()]
 
-spelarens_summa = beräkna_summa(spelarens_kort)
-datorns_summa = beräkna_summa(datorns_kort)
-
-print("Spelarens kort:", spelarens_kort, "Summa:", spelarens_summa)
-print("Datorns kort:", [datorns_kort[0]], "+ ?")
-
-if datorns_summa == 21:
-    print("Datorn har 21! Spelet är slut.")
-    exit()
-
-if spelarens_summa == 21:
-    print("Blackjack! Du vinner!")
-    exit()
-
-while spelarens_summa < 21:
-    val = input("Vill du ta ett till kort? (j/n): ").lower()
-    if val == "j":
-        nytt_kort = kortlek.pop()
-        spelarens_kort.append(nytt_kort)
         spelarens_summa = beräkna_summa(spelarens_kort)
-        print("Dina kort:", spelarens_kort, "Summa:", spelarens_summa)
+        datorns_summa = beräkna_summa(datorns_kort)
 
-        if spelarens_summa > 21:
-            print("Du blev tjock! Datorn vinner.")
-            exit()
-    else:
-        break
+        print("\nSpelarens kort:", spelarens_kort, "Summa:", spelarens_summa)
+        print("Datorns kort:", [datorns_kort[0]], "+ ?")
 
-print("\nDatorns kort:", datorns_kort, "Summa:", datorns_summa)
+        if datorns_summa == 21:
+            print("Datorn har 21! Spelet är slut.")
+        elif spelarens_summa == 21:
+            print("Blackjack! Du vinner!")
+        else:
+            while spelarens_summa < 21:
+                val = input("Vill du ta ett till kort? (j/n): ")
+                if val == "j":
+                    nytt_kort = kortlek.pop()
+                    spelarens_kort.append(nytt_kort)
+                    spelarens_summa = beräkna_summa(spelarens_kort)
+                    print("\nDina kort:", spelarens_kort, "Summa:", spelarens_summa)
+                    if spelarens_summa > 21:
+                        print("Du blev tjock! Datorn vinner!")
+                        break
+                else:
+                    break
 
-while datorns_summa < spelarens_summa and datorns_summa < 17:
-    nytt_kort = kortlek.pop()
-    datorns_kort.append(nytt_kort)
-    datorns_summa = beräkna_summa(datorns_kort)
-    print("Datorn drar ett kort:", nytt_kort, "\nDatorns nya summa:", datorns_summa)
+            if spelarens_summa <= 21:
+                print("\nDatorns kort:", datorns_kort, "Summa:", datorns_summa)
+                while datorns_summa < spelarens_summa and datorns_summa < 17:
+                    nytt_kort = kortlek.pop()
+                    datorns_kort.append(nytt_kort)
+                    datorns_summa = beräkna_summa(datorns_kort)
+                    print("Datorn drar ett kort:", nytt_kort)
+                    print("Datorns nya summa:", datorns_summa)
 
-if datorns_summa > 21:
-    print("\nDatorn blev tjock! Du vinner!")
-elif datorns_summa > spelarens_summa:
-    print("\nDatorn vinner! Bättre lycka nästa gång.")
-elif datorns_summa < spelarens_summa:
-    print("\nGrattis! Du vinner!")
-else:
-    print("\nDet blev oavgjort!")
+                if datorns_summa > 21:
+                    print("\nDatorn blev tjock! Du vinner!")
+                elif datorns_summa > spelarens_summa:
+                    print("\nDatorn vinner! Bättre lycka nästa gång.")
+                elif datorns_summa < spelarens_summa:
+                    print("\nGrattis! Du vinner!")
+                else:
+                    print("\nDet blev oavgjort!")
+        
+        spela_igen = input("\nVill du spela igen? (j/n):")
+        if spela_igen != "j":
+            break
 
-#Gör så att man kan köra igen#
-#Gör så att man kan vinna eller förlora pengar, man har också en plånbok#
+print("\nJasså du är här igen, vad roligt!")
+spela_blackjack()
--- readme.md
+++ readme.md
@@ -1,36 +1,41 @@
-2025-01-14
-Påbörjade utvecklingen av spelet genom att skapa en kortlek bestående av 24 uppsättningar av varje korttyp, vilket speglar verkligheten där man använder sig utav 6 kortlekar. Implementerade därefter logiken för att ge spelaren och datorn två kort var vid spelets början.
+2025-03-11
+Skapade funktionen spela_blackjack() så att man får valet att spela en omgång till. Förbättrade även strukturen på det som skrivs ut, så att det blir enklare att läsa av informationen i terminalen.
 
-Genomförde tester för att se till att korten delas ut slumpmässigt och visas korrekt.
+Genomförde även tester på den nya funktionen så att det inte uppstod några nya problem, beroende på om spelaren/datorn vann, det blev blackjack för spelaren/datorn eller om det blev lika.
 
-2025-01-20
-Skapade funktionen beräkna_summa() för att räkna ut summan av en spelares hand, med särskild hantering av klädda kort och ess. Införde även en första utskrift där spelarens kort och poäng visas, medan datorns andra kort döljs.
+2025-03-10
 
-Testade funktionen för att säkerställa att kortvärdena beräknas korrekt.
+2025-03-04
+
+2025-03-03
+
+2025-02-25
+
+2025-02-24
+
+2025-02-18
+
+2025-02-17
+
+2024-02-04
+
+2025-02-03
+
+2025-01-28
+
+2025-01-27
 
 2025-01-21
 Justerade beräkna_summa() så att ess korrekt kan räknas både som 1 och 11 beroende på handens sammanlagda värde. Implementerade även en omedelbar vinst- eller förlustutgång om spelaren eller datorn får exakt 21 poäng vid första utdelningen.
 
 Testade spelets startfas upprepade gånger för att identifiera och åtgärda eventuella fel i logiken.
 
-2025-01-27
+2025-01-20
+Skapade funktionen beräkna_summa() för att räkna ut summan av en spelares hand, med särskild hantering av klädda kort och ess. Införde även en första utskrift där spelarens kort och poäng visas, medan datorns andra kort döljs.
 
-2025-01-28
+Testade funktionen för att säkerställa att kortvärdena beräknas korrekt.
 
-2025-02-03
+2025-01-14
+Påbörjade utvecklingen av spelet genom att skapa en kortlek bestående av 24 uppsättningar av varje korttyp, vilket speglar verkligheten där man använder sig utav 6 kortlekar. Implementerade därefter logiken för att ge spelaren och datorn två kort var vid spelets början.
 
-2024-02-04
-
-2025-02-17
-
-2025-02-18
-
-2025-02-24
-
-2025-02-25
-
-2025-03-03
-
-2025-03-04
-
-2025-03-10
+Genomförde tester för att se till att korten delas ut slumpmässigt och visas korrekt.
